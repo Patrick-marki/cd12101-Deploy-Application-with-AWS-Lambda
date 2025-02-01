b58aa24ed3bd27a4f400bc7ed1281db3aca51c15
Patrick Marquart <patrick.marquart@zf.com>
2025-02-01 20:39:24 +0100

    added config.ts file for submission

---
+++ starter/client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'wuhdfl8ig7'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-k3w6v0r53tev4jq2.us.auth0.com',            // Auth0 domain
+  clientId: 'M9k5XO0VTP8iF9OnJFiTKFwcHFGULuUP',        // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
